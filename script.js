4490ef8965befaaa7fb96778b5e7781c04715aa2
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2025-03-20 15:25:35 +0100

    🚀 Second support JS - Ready to test

---
+++ script.js
@@ -0,0 +1,52 @@
+// ? Introduction :
+
+// Tu vas devoir relier ton fichier HTML avec ton fichier JS.
+// Tu auras réussi lorsqu'après avoir cliqué sur "Go Live", tu verras apparaître l'alerte ci-dessous !
+
+alert("Bravo, tu as relié ton fichier HTML avec JS 🎉");
+
+// ? Premier exercice : 
+// Créé ci-dessous une variable avec le mot clé const. Le nom de cette variable doit être yourFirstname.
+// La valeur de cette variable sera ton prénom.
+
+
+
+// ? Deuxième exercice :
+// Créé ci-dessous une variable avec le mot clé const, dont le nom doit être yourLastname, et dont la valeur sera ton prénom.
+
+
+
+// ? Troisième exercice :
+// Tu vas devoir injecter ta variable yourFirstname dans l'alert présente ci-dessous :
+// Cela aura pour effet d'afficher la valeur (c'est-à-dire ton prénom) dans le navigateur 😉
+
+alert();
+
+// ? Quatrième exercice :
+// L'alerte ci-dessous doit faire apparaître un message de bienvenue ET ta variable yourFirsname :
+// ! Attention : Tu ne dois pas créer deux alertes, mais bien une seule !
+
+alert();
+
+// ? Cinquième exercice :
+// L'alerte ci-dessous doit faire apparaître un message de bienvenue, ta variable yourFirsname et ta variable yourLastname :
+
+alert();
+
+// ? Sixième exercice :
+// L'alerte ci-dessous doit faire apparaitre la longueur de ton prénom.
+// ! A noter : tu ne dois pas compter et mettre un chiffre. 
+// ! Tu vas devoir utiliser une méthode applicable aux chaînes de caractères permettant de connaître la longueur d'un mot 😉
+
+alert("Longueur de mon prénom : ", /* supprime ce commentaire et rajoute la ligne attendue) */);
+
+// ? Septième exercice :
+// Créé ci-dessous une instruction conditionnelle (if / else).
+// Tu vas devoir comparer la longueur de ton prénom et de ton nom.
+
+// Si le prénom est plus long que le nom, renvoie une alerte disant "Ton prénom est plus grand que ton nom !"
+// Si le nom est plus grand que le prénom, renvoie une alerte disant "Ton nom est plus grand que ton prénom !"
+// Si le prénom et le nom sont de même longueur, renvoie "Ton prénom et ton nom sont de même longueur !".
+
+// ? Huitième exercice (🥵) :
+// Tu vas devoir boucler sur ton prénom, et compter le nombre de fois où la lettre "a" apparaît.
